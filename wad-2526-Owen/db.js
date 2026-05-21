ff41a5ef108e59e8cac9c7080abd1ced40e009c2
owen1313320-debug <owen.1313320@penabur.sch.id>
2026-05-21 16:01:11 +0700

    Add files via upload

---
+++ wad-2526-Owen/db.js
@@ -0,0 +1,54 @@
+const menus = [{
+    name: 'Coffee',
+    description: "The best coffee you can have from the land of Indonesia",
+    variants: [{
+        price: 25,
+        description: 'Americano',
+        stock: 10,
+    }, {
+        price: 30,
+        description: 'Latte',
+        stock: 7,
+    }],
+    photoUrl: "assets/coffee.jpg"
+}, {
+    name: "Seasonal Wine",
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 100,
+        description: 'Glass',
+        stock: 5,
+    }, {
+        price: 1500,
+        description: 'Bottle',
+        stock: 3,
+    }],
+    photoUrl: "assets/wine.jpg"
+}, {
+    name: "Triple Cheese Pizza",
+    description: "Indulge yourself in this simple yet delicious delicacy",
+    variants: [{
+        price: 15,
+        description: "Slice",
+        stock: 1,
+    }],
+    photoUrl: "assets/pizza.jpg",
+}, {
+    name: 'Avocado Pesto Salad',
+    description: "A great meal becomes perfect when combined with a good wine",
+    variants: [{
+        price: 35,
+        description: '',
+        stock: 2,
+    }, {
+        price: 45,
+        description: '+Tuna',
+        stock: 4,
+    }, {
+        price: 55,
+        description: '+Grilled Salmon',
+        stock: 3,
+    }],
+    photoUrl: "assets/salad.jpg"
+}
+];
